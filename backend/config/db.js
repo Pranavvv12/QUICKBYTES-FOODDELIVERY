0d6b8a6f53acb95c1984730e144230a4330b1649
Pranav Kumar.M <121292280+Pranavvv12@users.noreply.github.com>
2025-03-20 20:57:02 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 
 export const connectDB= async()=>{
-    await mongoose.connect('mongodb+srv://userquickbytes:user123@cluster0.r50ov.mongodb.net/Quickbytes').then(()=>console.log("DB CONNECTED"));
+    await mongoose.connect('mongodb+srv://newUser:newPassword@cluster0.r50ov.mongodb.net/Quickbytes').then(()=>console.log("DB CONNECTED"));
 }
