16a248d3607eebd4d3bd1102504370869ddceb64
Pranav Kumar.M <121292280+Pranavvv12@users.noreply.github.com>
2025-03-20 20:58:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`);
 });
-
-//mongodb+srv://userquickbytes:user123@cluster0.r50ov.mongodb.net/?
